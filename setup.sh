72cd2d12590f571499532b77469ec52e0946cf05
Ubuntu <ubuntu@ip-172-31-9-14.us-west-2.compute.internal>
2013-07-02 19:38:23 +0000

    Purge emacs

--- setup.sh
+++ setup.sh
@@ -22,9 +22,10 @@ sudo apt-get install -y rlwrap
 
 # Install emacs24
 # https://launchpad.net/~cassou/+archive/emacs
-sudo apt-add-repository -y ppa:cassou/emacs
-sudo apt-get update
-sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
+#emacs is bloatware
+#sudo apt-add-repository -y ppa:cassou/emacs
+#sudo apt-get update
+#sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
 
 # git pull and install dotfiles as well
 cd $HOME
